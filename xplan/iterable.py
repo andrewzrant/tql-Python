8273a73fcccc071cfbb90564d7ece067051bea75
yuanjie <313303303@qq.com>
2018-12-18 10:42:21 +0800

    pandas

--- xplan/iterable.py
+++ xplan/iterable.py
@@ -26,36 +26,51 @@ except:
 else:
     from tqdm import tqdm_notebook as tqdm
 
+# 统计函数: 待补充groupby.agg
+xsummary = X(lambda iterable: DataFrameSummary(iterable | xDataframe)['iterable'])
+xvalue_counts = X(lambda iterable, name='iterable': (iterable | xSeries(name=name)).value_counts())
+
+__funcs = [sum, min, max, abs, len, np.mean, np.median]
+xsum, xmin, xmax, xabs, xlen, xmean, xmedian = [X(i) for i in __funcs]
+
+xnorm = X(lambda iterable, ord=2: np.linalg.norm(iterable, ord))
+xcount = X(lambda iterable: Counter(iterable))
+
+xunique = X(lambda iterable: list(OrderedDict.fromkeys(iterable)))  # 移除列表中的重复元素(保持有序)
+xsort = X(lambda iterable, reverse=False, key=None: sorted(iterable, key=key, reverse=reverse))
+
+xmax_index = X(lambda x: max(range(len(x)), key=x.__getitem__))  # 列表中最小和最大值的索引
+xmin_index = X(lambda x: min(range(len(x)), key=x.__getitem__))  # 列表中最小和最大值的索引
+xmost_freq = X(lambda x: max(set(x), key=x.count))  # 查找列表中频率最高的值, key作用于set(x), 可类推出其他用法
+
+# print
+xprint = X(pprint)
+xtqdm = X(lambda iterable, desc=None: tqdm(iterable, desc))
+
 # base types
 xtuple, xlist, xset = X(tuple), X(list), X(set)
 
-xseries = X(lambda iterable, name='iterable': pd.Series(iterable, name=name))
-xdataframe = X(lambda iterable, name='iterable': pd.DataFrame({name: iterable}))
+
+@X
+def xSeries(iterable, name='iterable'):
+    if isinstance(iterable, pd.Series):
+        return iterable
+    else:
+        return pd.Series(iterable, name=name)
+
+
+@X
+def xDataframe(iterable, name='iterable'):
+    if isinstance(iterable, pd.DataFrame):
+        return iterable
+    else:
+        return pd.DataFrame({name: iterable})
+
 
 # 高阶函数
 xmap = X(lambda iterable, func: map(func, iterable))
 xreduce = X(lambda iterable, func: reduce(func, iterable))
 xfilter = X(lambda iterable, func: filter(func, iterable))
-
-# 统计函数: 待补充groupby.agg
-xsummary = X(lambda iterable: DataFrameSummary(iterable | xdataframe)['iterable'])
-funcs = [sum, min, max, abs, len, np.mean, np.median]
-xsum, xmin, xmax, xabs, xlen, xmean, xmedian = [X(i) for i in funcs]
-
-xnorm = X(lambda iterable, ord=2: np.linalg.norm(iterable, ord))
-xcount = X(lambda iterable: Counter(iterable))
-xcounts = X(lambda iterable, name='iterable': (iterable | xseries(name=name)).value_counts())
-xunique = X(lambda iterable: list(OrderedDict.fromkeys(iterable)))  # 移除列表中的重复元素(保持有序)
-xsort = X(lambda iterable, reverse=False, key=None: sorted(iterable, key=key, reverse=reverse))
-
-max_index = X(lambda x: max(range(len(x)), key=x.__getitem__))  # 列表中最小和最大值的索引
-min_index = X(lambda x: min(range(len(x)), key=x.__getitem__))  # 列表中最小和最大值的索引
-most_freq = X(lambda x: max(set(x), key=x.count))  # 查找列表中频率最高的值, key作用于set(x), 可类推出其他用法
-
-
-# print
-xprint = X(pprint)
-xtqdm = X(lambda iterable, desc=None: tqdm(iterable, desc))
 
 
 # multiple
