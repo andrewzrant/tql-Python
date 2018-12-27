eb9a1bedf282df36dee638dee4aae88362ae9b39
尐 <313303303@qq.com>
2018-12-27 12:00:34 +0800

    Create doc2vec.py

---
+++ xplan/nlp_utils/doc2vec.py
@@ -0,0 +1,34 @@
+import numpy as np
+from tqdm import tqdm
+
+
+class Doc2Vec(object):
+
+    def __init__(self, fname):
+        self.embeddings = self._load_wv(fname)
+
+    def sent2vec(self, sentence, tokenizer, mode='sum', normalize=True):
+
+        words = self.tokenizer(sentence)
+        words = [w for w in words if w in self.embeddings]
+
+        if words:
+            if mode == 'sum':
+                _ = np.array([self.embeddings[w] for w in words]).sum(axis=0)
+                return _ / np.sqrt(np.clip((_ ** 2).sum(), 1e-12, None)) if normalize else _
+            elif mode == 'mean':
+                _ = np.array([self.embeddings[w] for w in words]).mean(axis=0)
+                return _ / np.sqrt(np.clip((_ ** 2).sum(), 1e-12, None)) if normalize else _
+            else:
+                return np.zeros(self.word_size)
+        else:
+            return np.zeros(self.word_size)
+
+    def _load_wv(self, fname):
+        self.embeddings = {}
+        with open(fname) as f:
+            for line in tqdm(f, 'Load Vectors ...'):
+                line = line.split()
+                if len(line) > 2:
+                    self.embeddings[line[0]] = np.asarray(line[1:], dtype='float32')
+            self.word_size = len(line[1:])
