d8810f6394c68d1557df8d7757434a7d36a1f3b5
yuanjie <313303303@qq.com>
2019-01-09 22:31:00 +0800

    update

--- xplan/models/classifier/bayes_opt_lgb.py
+++ xplan/models/classifier/bayes_opt_lgb.py
@@ -8,6 +8,7 @@ __mtime__ = '19-1-3'
 import warnings
 
 warnings.filterwarnings('ignore')
+import pandas as pd
 import lightgbm as lgb
 from bayes_opt import BayesianOptimization
 from bayes_opt.observer import JSONLogger
@@ -21,8 +22,24 @@ class BayesOptLGB(object):
     opt_lgb.get_best_model()
     """
 
-    def __init__(self, X, y, categorical_feature='auto'):
+    def __init__(self, X, y, topk=10, categorical_feature='auto', metric='auc', fix_params={'min_child_weight': 0.001}):
         self.data = lgb.Dataset(X, y, categorical_feature=categorical_feature, free_raw_data=False)
+        self.topk = topk
+        self.metric = metric
+        self.fix_params = fix_params
+        self.params_ls = []
+        self.params_ls_sk = []
+        self.params_best = {}
+        self.params_best_sk = {}
+        self.params_opt = None
+        self.__iteration = {}
+
+        if self.fix_params:
+            print('\033[94m%s\033[0m\n' % " Fix min_child_weight ...")
+
+    @property
+    def get_best_model(self, best_iter):
+        return lgb.train(self.params, self.data, best_iter)
 
     def run(self, n_iter=10, save_log=False):
         logger = JSONLogger(path="./opt_lgb_logs.json")
@@ -30,7 +47,7 @@ class BayesOptLGB(object):
         BoParams = {
             'num_leaves': (2 ** 5, 2 ** 16),
             'min_split_gain': (0.01, 1),
-            'min_child_weight': (0, 0.01),  # 0.001可以考虑不调?
+            'min_child_weight': (0, 0.01),
             'min_child_samples': (8, 32),
             'subsample': (0.6, 1),
             'colsample_bytree': (0.6, 1),
@@ -40,24 +57,41 @@ class BayesOptLGB(object):
         optimizer = BayesianOptimization(self.__evaluator, BoParams)
         if save_log:
             optimizer.subscribe(Events.OPTMIZATION_STEP, logger)
+
+        if self.fix_params:
+            optimizer.set_bounds({k: (v, v) for k, v in self.fix_params.items()})
+
+        optimizer.probe(
+            {'num_leaves': 2 ** 7 - 1,
+             'min_split_gain': 0,
+             'min_child_weight': 0.001,
+             'min_child_samples': 6,
+             'subsample': 0.8,
+             'colsample_bytree': 0.8,
+             'reg_alpha': 0.01,
+             'reg_lambda': 1})
+
         gp_params = {"alpha": 1e-5, "n_restarts_optimizer": 2}
         optimizer.maximize(init_points=3, n_iter=n_iter, acq='ucb', kappa=2.576, xi=0.0, **gp_params)
         self.best_params = optimizer.max
         self.__get_params()
 
-    def get_best_model(self, best_iter):
-        return lgb.train(self.params, self.data, best_iter)
+        self.params_opt = (
+            pd.concat([pd.DataFrame(self.__iteration), pd.DataFrame(optimizer.res)], 1)
+                .sort_values('target', ascending=False)
+                .reset_index(drop=True)[:self.topk])
+        self.__get_params()
 
     def __evaluator(self, num_leaves, min_split_gain, min_child_weight, min_child_samples, subsample, colsample_bytree,
                     reg_alpha, reg_lambda):
-        params = dict(
+        self.__params_sk = dict(
             boosting_type='gbdt',
             objective='binary',
             max_depth=-1,
             num_leaves=int(num_leaves),
             learning_rate=0.01,
             min_split_gain=min_split_gain,
-            min_child_weight=min_child_weight,  # 可以固定?
+            min_child_weight=min_child_weight,
             min_child_samples=int(min_child_samples),
             subsample=subsample,
             subsample_freq=8,
@@ -65,37 +99,30 @@ class BayesOptLGB(object):
             reg_alpha=reg_alpha,
             reg_lambda=reg_lambda,
             scale_pos_weight=1,
-            random_state=None,  ##
+            random_state=0,
             n_jobs=-1
         )
+        params = self.__params_sk.copy()
+        # params['metric'] = self.metric
 
-        metric = 'auc'
-        cv_rst = lgb.cv(params, self.data, num_boost_round=3000, nfold=5, metrics=metric, early_stopping_rounds=100,
-                        verbose_eval=None, show_stdv=False)
+        cv_rst = lgb.cv(params, self.data, num_boost_round=3000, nfold=5, early_stopping_rounds=100,
+                        metrics=self.metric, verbose_eval=None, show_stdv=False)
 
-        _ = cv_rst['%s-mean' % metric]
-        print('\nBest Iter: %s' % len(_))
+        _ = cv_rst['%s-mean' % self.metric]
+        self.__iteration.setdefault('best_iteration', []).append(len(_))
         return _[-1]
 
     def __get_params(self):
-        params = {'boosting_type': 'gbdt',
-                  'objective': 'binary',
-                  'max_depth': -1,
-                  'num_leaves': 127,
-                  'learning_rate': 0.01,
-                  'min_split_gain': 0.0,
-                  'min_child_weight': 0.001,
-                  'min_child_samples': 20,
-                  'subsample': 0.8,
-                  'subsample_freq': 8,
-                  'colsample_bytree': 0.8,
-                  'reg_alpha': 0.0,
-                  'reg_lambda': 0.0,
-                  'scale_pos_weight': 1,
-                  'random_state': None,  ##
-                  'n_jobs': -1}
-        params.update(self.best_params['params'])
-        params['num_leaves'] = int(params['num_leaves'])
-        params['min_child_samples'] = int(params['min_child_samples'])
-        self.params = {k: float('%.3f' % v) if isinstance(v, float) else v for k, v in params.items()}
-        self.params_sk = self.params.copy()
+        for _, (i, p, _) in self.params_opt.iterrows():
+            params_sk = {**p, **{'n_estimators': i}, **self.__params_sk}
+            params_sk['num_leaves'] = int(params_sk['num_leaves'])
+            params_sk['min_child_samples'] = int(params_sk['min_child_samples'])
+            params_sk = {k: float('%.3f' % v) if isinstance(v, float) else v for k, v in params_sk.items()}
+            self.params_ls_sk.append(params_sk)
+
+            params = params_sk.copy()
+            num_boost_round = params.pop('n_estimators')
+            self.params_ls.append({'params': params, 'num_boost_round': num_boost_round})
+
+        self.params_best = self.params_ls[0]
+        self.params_best_sk = self.params_ls_sk[0]
--- xplan/models/classifier/bayes_opt_xgb.py
+++ xplan/models/classifier/bayes_opt_xgb.py
@@ -8,6 +8,8 @@ __mtime__ = '19-1-3'
 import warnings
 
 warnings.filterwarnings('ignore')
+
+import pandas as pd
 import xgboost as xgb
 from bayes_opt import BayesianOptimization
 from bayes_opt.observer import JSONLogger
@@ -20,88 +22,115 @@ class BayesOptXGB(object):
     opt_xgb.run(3)
     opt_xgb.get_best_model()
     """
+    print('\033[94m%s\033[0m\n' % " Fix min_child_weight ...")
 
-    def __init__(self, X, y, missing=None):
-        self.data = xgb.DMatrix(X, y, missing=missing)
 
-    def run(self, n_iter=10, save_log=False):
-        logger = JSONLogger(path="./opt_xgb_logs.json")
+def __init__(self, X, y, topk=10, missing=None, metric='auc', fix_params={}):
+    self.data = xgb.DMatrix(X, y, missing=missing)
+    self.topk = topk
+    self.metric = metric
+    self.fix_params = fix_params  # 固定不需要调节的参数
+    self.params_ls = []
+    self.params_ls_sk = []
+    self.params_best = {}
+    self.params_best_sk = {}
+    self.params_opt = None
+    self.__iteration = {}
 
-        BoParams = {
-            'max_depth': (5, 16),
-            'min_child_weight': (1, 10),
-            'gamma': (0.01, 1),
-            'subsample': (0.6, 1),
-            'colsample_bytree': (0.6, 1),
-            'reg_alpha': (0, 1),
-            'reg_lambda': (0, 1),
-        }
-        optimizer = BayesianOptimization(self.__evaluator, BoParams)
-        if save_log:
-            optimizer.subscribe(Events.OPTMIZATION_STEP, logger)
-        gp_params = {"alpha": 1e-5, "n_restarts_optimizer": 2}
-        optimizer.maximize(init_points=3, n_iter=n_iter, acq='ucb', kappa=2.576, xi=0.0, **gp_params)
-        self.best_params = optimizer.max
-        self.__get_params()
+    if self.fix_params:
+        print('\033[94m%s\033[0m\n' % " Fix min_child_weight ...")
 
-    def get_best_model(self, best_iter):
-        return xgb.train(self.params, self.data, best_iter)
 
-    def __evaluator(self, max_depth, gamma, min_child_weight, subsample, colsample_bytree,
-                    reg_alpha, reg_lambda):
-        params = dict(
-            silent=True,
-            booster='gbtree',
-            objective='binary:logistic',
-            max_depth=int(max_depth),
-            learning_rate=0.01,
-            gamma=gamma,  # min_split_gain
-            min_child_weight=min_child_weight,
-            subsample=subsample,
-            colsample_bylevel=0.8,  # 每一层的列数
-            colsample_bytree=colsample_bytree,
-            reg_alpha=reg_alpha,
-            reg_lambda=reg_lambda,
-            scale_pos_weight=1,
-            random_state=0,
-            n_jobs=-1
-        )
-        params['eta'] = params.pop('learning_rate')
+@property
+def best_model(self):
+    if self.params_best:
+        return xgb.train(dtrain=self.data, **self.params_best)
+    else:
+        print("Please Run !")
+
+
+def run(self, n_iter=10, save_log=False):
+    logger = JSONLogger(path="./opt_xgb_logs.json")
+
+    BoParams = {
+        'max_depth': (5, 16),
+        'min_child_weight': (1, 10),
+        'gamma': (0, 1),
+        'subsample': (0.6, 1),
+        'colsample_bytree': (0.6, 1),
+        'reg_alpha': (0, 1),
+        'reg_lambda': (0, 1),
+    }
+    optimizer = BayesianOptimization(self.__evaluator, BoParams)
+    if save_log:
+        optimizer.subscribe(Events.OPTMIZATION_STEP, logger)
+
+    if self.fix_params:
+        optimizer.set_bounds({k: (v, v) for k, v in self.fix_params.items()})
+
+    optimizer.probe(
+        {'max_depth': 7,
+         'min_child_weight': 1,
+         'gamma': 0,
+         'subsample': 0.8,
+         'colsample_bytree': 0.8,
+         'reg_alpha': 0.01,
+         'reg_lambda': 1})
+
+    gp_params = {"alpha": 1e-5, "n_restarts_optimizer": 3}
+    optimizer.maximize(init_points=3, n_iter=n_iter, acq='ucb', kappa=2.576, xi=0.0, **gp_params)
+
+    self.params_opt = (
+        pd.concat([pd.DataFrame(self.__iteration), pd.DataFrame(optimizer.res)], 1)
+            .sort_values('target', ascending=False)
+            .reset_index(drop=True)[:self.topk])
+    self.__get_params()
+
+
+def __evaluator(self, max_depth, gamma, min_child_weight, subsample, colsample_bytree,
+                reg_alpha, reg_lambda):
+    self.__params_sk = dict(
+        silent=True,
+        booster='gbtree',
+        objective='binary:logistic',
+        max_depth=int(max_depth),
+        learning_rate=0.01,
+        gamma=gamma,  # min_split_gain
+        min_child_weight=min_child_weight,
+        subsample=subsample,
+        colsample_bylevel=0.8,  # 每一层的列数
+        colsample_bytree=colsample_bytree,
+        reg_alpha=reg_alpha,
+        reg_lambda=reg_lambda,
+        scale_pos_weight=1,
+        random_state=0,
+        n_jobs=-1
+    )
+    params = self.__params_sk.copy()
+    params['eta'] = params.pop('learning_rate')
+    params['alpha'] = params.pop('reg_alpha')
+    params['lambda'] = params.pop('reg_lambda')
+    # params['eval_metric'] = self.metric
+
+    cv_rst = xgb.cv(params, self.data, num_boost_round=3600, nfold=5, early_stopping_rounds=100,
+                    metrics=self.metric, stratified=True, show_stdv=False, as_pandas=False)
+    _ = cv_rst['test-%s-mean' % self.metric]
+    self.__iteration.setdefault('best_iteration', []).append(len(_))
+    return _[-1]
+
+
+def __get_params(self):
+    for _, (i, p, _) in self.params_opt.iterrows():
+        params_sk = {**p, **{'n_estimators': i}, **self.__params_sk}
+        params_sk['max_depth'] = int(params_sk['max_depth'])
+        params_sk = {k: float('%.3f' % v) if isinstance(v, float) else v for k, v in params_sk.items()}
+        self.params_ls_sk.append(params_sk)
+
+        params = params_sk.copy()
         params['alpha'] = params.pop('reg_alpha')
         params['lambda'] = params.pop('reg_lambda')
+        num_boost_round = params.pop('n_estimators')
+        self.params_ls.append({'params': params, 'num_boost_round': num_boost_round})
 
-        metric = 'auc'
-        cv_rst = xgb.cv(params, self.data, num_boost_round=3600, nfold=5, metrics=metric, early_stopping_rounds=100,
-                        verbose_eval=None, stratified=True, show_stdv=False, as_pandas=False)
-
-        _ = cv_rst['test-%s-mean' % metric]
-        print('\nBest Iter: %s' % len(_))
-        return _[-1]
-
-    def __get_params(self):
-        params = {'booster': 'gbtree',
-                  'objective': 'binary:logistic',
-                  'max_depth': 7,
-                  'gamma': 0.0,
-                  'min_child_weight': 1,
-                  'subsample': 0.8,
-                  'colsample_bytree': 0.8,
-                  'colsample_bylevel': 0.8,
-                  'scale_pos_weight': 1,
-                  'random_state': 0,
-                  'n_jobs': -1,
-                  'silent': True,
-                  'eta': 0.01,
-                  'alpha': 0.0,
-                  'lambda': 0.0}
-        _params = self.best_params['params']
-        _params['alpha'] = _params.pop('reg_alpha')
-        _params['lambda'] = _params.pop('reg_lambda')
-
-        params.update(self.best_params['params'])
-        params['max_depth'] = int(params['max_depth'])
-        self.params = {k: float('%.3f' % v) if isinstance(v, float) else v for k, v in params.items()}
-        self.params_sk = self.params.copy()
-        self.params_sk['learning_rate'] = self.params_sk.pop('eta')
-        self.params_sk['reg_alpha'] = self.params_sk.pop('alpha')
-        self.params_sk['reg_lambda'] = self.params_sk.pop('lambda')
+    self.params_best = self.params_ls[0]
+    self.params_best_sk = self.params_ls_sk[0]
