bc6a19da872850fe3ee5caf1109a3eb2261ad2d7
yuanjie <313303303@qq.com>
2019-01-11 16:47:50 +0800

    update

--- xplan/models/semi/semi_simple.py
+++ xplan/models/semi/semi_simple.py
@@ -15,36 +15,53 @@ from ..classifier import BayesOptLGB
 
 class SemiSimple(object):
 
-    def __init__(self, subsample=0.05, n_iter=1):
+    def __init__(self, subsample=0.05, n_iter=1, scale_pos=1, mode=None):
+        """
+
+        :param subsample:
+        :param n_iter:
+        :param scale_pos: 正样本 / 负样本
+        :param mode:
+            'p': 从X_test, 只采样正样本
+            'n': 从X_test, 只采样正样本
+            None: 从X_test, 采样正+负样本
+        """
         self.subsample = subsample
         self.n_iter = n_iter
-        _ = "X_train will stack {:.2f} % of X_test".format((1 - (1 - 2 * self.subsample) ** n_iter) * 100)
+        self.scale_pos = scale_pos
+        self.mode = mode
+        _ = "X_train will stack ≈ {:.2f} % of X_test".format((1 - (1 - 2 * self.subsample) ** n_iter) * 100)
         Cprint().cprint(_)
 
     def fit(self, X_train, y_train, X_test):
-        X_test = np.asarray(X_test)
+        self.X_train = X_train
+        self.y_train = y_train
+        self.X_test = np.asarray(X_test)
 
-        # score = [] # 判断停止
         for _ in tqdm(range(self.n_iter + 1)):
-            ##############可以定义其他模型#################
+            ################可以定义其他模型#################
             # self.clf.fit(X_train, y_train)
-            bo = BayesOptLGB(X_train, y_train)
+            bo = BayesOptLGB(self.X_train, self.y_train)
             bo.run()
-            # score.append()
             self.clf = LGBMClassifier(**bo.params_best_sk)
-            self.clf.fit(X_train, y_train)
-            ############################################
+            self.clf.fit(self.X_train, self.y_train)
+            ##############################################
 
-            pred = (
-                pd.Series(self.clf.predict_proba(X_test)[:, 1])
-                    .mask(lambda x: x < x.quantile(self.subsample), 0)
-                    .mask(lambda x: x > x.quantile(1 - self.subsample), 1)
-            )
+            _ = pd.Series(self.clf.predict_proba(self.X_test)[:, 1])
+
+            if self.mode == 'n':
+                pred = _.mask(lambda x: x < x.quantile(self.subsample), 0)
+            elif self.mode == 'p':
+                pred = _.mask(lambda x: x > x.quantile(1 - self.subsample * self.scale_pos), 1)
+            else:
+                pred = (_.mask(lambda x: x < x.quantile(self.subsample), 0)
+                        .mask(lambda x: x > x.quantile(1 - self.subsample * self.scale_pos), 1))
+
             pred_ = pred[lambda x: x.isin([0, 1])]
-
             pseudo_label_idx = pred_.index
             no_pseudo_label_idx = pred.index.difference(pseudo_label_idx)
-            X_train = np.row_stack((X_train, X_test[pseudo_label_idx]))
-            y_train = np.hstack((y_train, pred_))
-            X_test = X_test[no_pseudo_label_idx]
+
+            self.X_train = np.row_stack((self.X_train, self.X_test[pseudo_label_idx]))
+            self.y_train = np.hstack((self.y_train, pred_))
+            self.X_test = self.X_test[no_pseudo_label_idx]
         return self.clf
