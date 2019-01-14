f41da5eaf0f9405109cfac7087226e569dedd89a
yuanjie <313303303@qq.com>
2019-01-14 11:24:09 +0800

    update

---
+++ xplan/utils_sh/install_lgb.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+git clone --recursive https://github.com/Microsoft/LightGBM ; cd LightGBM
+mkdir build ; cd build
+cmake ..
+make -j4
+cd ../python-package
+python setup.py
---
+++ xplan/utils_sh/install_xgb.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+git clone --recursive https://github.com/dmlc/xgboost
+cd xgboost
+mkdir build
+cd build
+cmake .. -DUSE_CUDA=ON
+make -j4
+cd python-package
+python setup.py install
