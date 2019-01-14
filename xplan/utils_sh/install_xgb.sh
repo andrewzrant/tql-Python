f3b7e5b6ab6dafb566eb2673cbc623676b6d949c
yuanjie <313303303@qq.com>
2019-01-14 11:24:37 +0800

    update

--- xplan/utils_sh/install_xgb.sh
+++ xplan/utils_sh/install_xgb.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
-git clone --recursive https://github.com/dmlc/xgboost
-cd xgboost
-mkdir build
-cd build
+git clone --recursive https://github.com/dmlc/xgboost; cd xgboost
+mkdir build; cd build
 cmake .. -DUSE_CUDA=ON
 make -j4
 cd python-package
