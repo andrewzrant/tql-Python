79580d3edc33778cfc5f8190ed1072960c473dcd
yuanjie <313303303@qq.com>
2019-01-14 21:15:12 +0800

    xx

--- git_push_install.sh
+++ git_push_install.sh
@@ -1,16 +1,13 @@
 #!/usr/bin/env bash
-while getopts ":a:b:c:" opt
+while getopts ":m:b:c:" opt
 do
     case $opt in
-        a)
-        echo "参数b的值$OPTARG"
-        ;;
+        m)
+        export m=$OPTARG;;
         b)
-        echo "参数b的值$OPTARG"
-        ;;
+        echo "参数b的值$OPTARG";;
         c)
-        echo "参数c的值$OPTARG"
-        ;;
+        echo "参数c的值$OPTARG";;
         ?)
         echo "未知参数"
         exit 1;;
@@ -19,6 +16,6 @@ done
 
 git pull
 git add *
-git commit -m 'update'
+git commit -m $m
 git push
 pip uninstall x-plan -y && pip install git+https://github.com/Jie-Yuan/X-plan.git -U --user
