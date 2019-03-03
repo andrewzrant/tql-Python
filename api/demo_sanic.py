30f12184a968f3614f917cf1b5df95686a212c48
yuanjie <313303303@qq.com>
2019-03-03 12:22:43 +0800

    mac update

--- api/demo_sanic.py
+++ api/demo_sanic.py
@@ -22,15 +22,15 @@ async def post(request):
     return json(request.json)
 
 
-def test():
-    import requests
-    r = requests.get('http://0.0.0.0:8000/get', params={'a': 1})
-    r.json()
-    # {'a': ['1']}
-
-    r = requests.post('http://0.0.0.0:8000/post', json={'a': 1})
-    r.json()
-    # {'a': 1}
+# def test():
+#     import requests
+#     r = requests.get('http://0.0.0.0:8000/get', params={'a': 1})
+#     r.json()
+#     # {'a': ['1']}
+#
+#     r = requests.post('http://0.0.0.0:8000/post', json={'a': 1})
+#     r.json()
+#     # {'a': 1}
 
 
 if __name__ == '__main__':
--- api/demo_vibora.py
+++ api/demo_vibora.py
@@ -5,46 +5,25 @@ __author__ = 'JieYuan'
 __mtime__ = '18-11-6'
 """
 import jieba
-from vibora.utils import RequestParams
-from vibora import Vibora, Response, Request
+import json
+from vibora import Vibora, Request
 from vibora.responses import JsonResponse
-from urllib import parse
-from vibora.router import RouterStrategy, uuid
+# from urllib import parse
+# parse.unquote(parse.quote('中文'))
 
-app = Vibora()  # Vibora(router_strategy=RouterStrategy.STRICT)
-
-from poetry.poetry_gen import PoetryGen
-p = PoetryGen(False)
-
-# @app.route('/aa/<corpus>', methods=['GET'])  # @app.route(re.compile('/product/(?P<product_id>[0-9]+)'))
-# async def page(corpus: str):
-#     corpus = parse.unquote(corpus)  # parse.unquote(parse.quote('中文'))
-#     _ = jieba.lcut(corpus)
-#     return JsonResponse({'中国': _})
+app = Vibora()
 
 
-# @app.route('/', methods=['POST'])
-# async def home(request: Request):
-#     uploaded_files = []
-#     for file in (await request.files):
-#         print(file)
-#         file.save('/tmp/' + str(uuid.uuid4()))
-#         print(f'Received uploaded file: {file.filename}')
-#         uploaded_files.append(file.filename)
-#     return JsonResponse(uploaded_files)
+@app.route('/json', methods=['POST'])
+async def home(request: Request):
+    content = await request.stream.read()
+    content = eval(content)
+    content = jieba.lcut(content.get('text', ''))
+    return JsonResponse(content)
 
-# @app.route('/')
-# async def home(request: Request):
-#     print(dict(request.args))
-#     return Response(f"Name: {request.args['name']}")
-
-
-@app.route('/poetry/<corpus>', methods=['GET'])  # @app.route(re.compile('/product/(?P<product_id>[0-9]+)'))
-async def page(corpus: str):
-    corpus = parse.unquote(corpus)  # parse.unquote(parse.quote('中文'))
-    _ = p.gen(start_words=corpus).split()
-    return JsonResponse({'中国': _})
-
+# import requests
+# r = requests.post('http://0.0.0.0:5000/json', json={'text': '小米公司还不错啊'})
+# r.json()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, workers=4, debug=True)
+    app.run(host='0.0.0.0', port=5000, workers=4)
