6e32bb69e389bc586d95b07003e404514dd2b631
yuanjie <313303303@qq.com>
2019-03-12 10:40:14 +0800

    nn

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("README.md", encoding='utf-8') as f:
 
 
 def get_requirements():
-    _ = 'requirements.txt'
+    _ = './requirements.txt'
     if os.path.isfile(_):
         with open(_, encoding='utf-8') as f:
             return f.read().split()
