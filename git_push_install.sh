c0469c8e2bddd0bfac7dc62171b332f580d232c0
yuanjie <313303303@qq.com>
2019-03-18 17:07:32 +0800

    oof

--- git_push_install.sh
+++ git_push_install.sh
@@ -18,7 +18,7 @@ git pull
 git add *
 git commit -m "$update_info"
 git push
-pip uninstall x-plan -y \
+pip uninstall yuan -y \
 && pip install git+https://github.com/Jie-Yuan/Yuan.git -U -i https://pypi.tuna.tsinghua.edu.cn/simple
 
 exit 0
