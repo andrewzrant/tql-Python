7f08c309b05805505e9f8abaa537b7bb10996940
yuanjie <313303303@qq.com>
2019-04-15 09:39:53 +0800

    x

--- email/__init__.py
+++ email/__init__.py
@@ -5,11 +5,11 @@ __title__ = '__init__.py'
 __author__ = 'JieYuan'
 __mtime__ = '2019/4/2'
 """
-"""https://sender.readthedocs.io/"""
-from sender import Mail
-
-mail = Mail()
-
-mail.send_message("Hello",
-                  fromaddr="from@example.com",
-                  to="yuanjie@xiaomi.com", body="Hello world!")
+# """https://sender.readthedocs.io/"""
+# from sender import Mail
+#
+# mail = Mail()
+#
+# mail.send_message("Hello",
+#                   fromaddr="from@example.com",
+#                   to="yuanjie@xiaomi.com", body="Hello world!")
