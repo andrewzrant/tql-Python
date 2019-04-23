c30b76cbd7b33ad54c51a33aa8a6380c42de0186
yuanjie <313303303@qq.com>
2019-04-23 22:03:17 +0800

    plt

--- yuan/pipe.py
+++ yuan/pipe.py
@@ -58,18 +58,16 @@ sns.set(style="darkgrid")  # darkgrid, whitegrid, dark, white,和ticks
 # sns.axes_style()
 
 # plt.style.use('ggplot')
-# plt.rcParams['font.sans-serif'] = ['Simhei']  # 中文乱码的处理
-# plt.rcParams['font.family'] = 'sans-serif'
-plt.rcParams['axes.unicode_minus'] = False
-# 负号
+plt.rcParams['font.sans-serif'] = ['Simhei']  # 中文乱码的处理
+plt.rcParams['font.family'] = 'sans-serif'
+plt.rcParams['axes.unicode_minus'] = False  # 负号
 plt.rcParams["text.usetex"] = False
 plt.rcParams["legend.numpoints"] = 1
 plt.rcParams["figure.figsize"] = (18, 9)  # (12, 6)
 plt.rcParams["figure.dpi"] = 128
 plt.rcParams["savefig.dpi"] = plt.rcParams["figure.dpi"]
-plt.rcParams["font.size"] = 10
+plt.rcParams["font.size"] = 12
 plt.rcParams["pdf.fonttype"] = 42
-
 
 
 #########################################################################
