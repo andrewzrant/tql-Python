e21c613a4fd03ca5e0876ac4a60e0946fca10057
yuanjie <313303303@qq.com>
2019-05-18 17:29:05 +0800

    bug

--- iwork/nlp/utils/__init__.py
+++ iwork/nlp/utils/__init__.py
@@ -7,5 +7,5 @@ from .WordVec2BOW import WordVec2BOW
 from .Emoji import Emoji
 from .ngrams import ngrams, everygrams, skipgrams
 from .VecQuery import VecQuery
-from .WordCloud import WordCloud
 from .WordExtractor import WordExtractor
+# from .WordCloud import WordCloud
