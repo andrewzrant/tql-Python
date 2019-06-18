487ceaa24e55a97958203f8cbc94ee6802af4267
yuanjie <313303303@qq.com>
2019-06-18 13:54:15 +0800

    count rank encoder

--- tql/features/transformer/CountEncoder.py
+++ tql/features/transformer/CountEncoder.py
@@ -5,10 +5,9 @@ __title__ = 'CountEncoder'
 __author__ = 'JieYuan'
 __mtime__ = '19-1-10'
 """
-import pandas as pd
-from ...pipe import tqdm
+from collections import OrderedDict
 
-from concurrent.futures import ProcessPoolExecutor
+import pandas as pd
 from sklearn.base import BaseEstimator, TransformerMixin
 
 
@@ -17,46 +16,20 @@ class CountEncoder(BaseEstimator, TransformerMixin):
     def __init__(self, normalize=False, dropna=False):
         self.dropna = dropna
         self.normalize = normalize
-        self.counter_ls = []
+        self.mapper = None
 
-    def fit(self, df: pd.DataFrame):
-        self.w = df.shape[1]
-        self.counter_ls = list(self._pe(self._fit, tqdm(self._get_series_ls(df), 'Fitting ...'), self.w))
+    def fit(self, s: pd.Series):
+        self.mapper = (s.value_counts(normalize=self.normalize, dropna=self.dropna)
+                       .to_dict(OrderedDict))
         return self
 
-    def transform(self, df):
-        assert self.w == df.shape[1]
-        _ = self._pe(self._transform, tqdm(zip(self.counter_ls, self._get_series_ls(df)), 'Transforming ...'), self.w)
-        return pd.concat(_, 1)
-
-    # def fit_transform(self, df):
-    #     return self.fit(df).transform(df)
-
-    def _fit(self, s: pd.Series):
-        _ = (s.value_counts(normalize=self.normalize, dropna=self.dropna)
-             .reset_index(name='count_' + s.name))
+    def transform(self, s: pd.Series):
+        _ = s.map(self.mapper).fillna(0)  # 不在训练集的补0
         return _
-
-    def _transform(self, args):
-        c, s = args
-        _ = (s.to_frame('index')
-             .merge(c, 'left')
-             .drop('index', 1)
-             .fillna(0))  # 不在训练集的补0
-        return _
-
-    def _pe(self, func, iterable, feat_num):
-        with ProcessPoolExecutor(5 if feat_num > 5 else feat_num) as pool:
-            return pool.map(func, iterable)
-
-    def _get_series_ls(self, df):
-        return [v for k, v in df.items()]
 
 
 if __name__ == '__main__':
-    df = pd.DataFrame({'a': ['1', '2'], 'b': ['2', '2']})
-    c = CountEncoder()
-    c.fit(df)
-    print(c)
-    print(c.transform(df))
-    print(c.fit_transform(df))
+    s = pd.Series(['a', 'a', 'a', 'b', 'b'])
+    ce = CountEncoder()
+    print(ce.fit_transform(s))
+    print(ce.mapper)
--- tql/features/transformer/RankEncoder.py
+++ tql/features/transformer/RankEncoder.py
@@ -14,17 +14,18 @@ from sklearn.base import BaseEstimator, TransformerMixin
 class RankEncoder(BaseEstimator, TransformerMixin):
 
     def __init__(self, topn=None):
-        self.topn = topn
+        self.topn = (topn if topn else 10000)
+        self.mapper = None
 
     def fit(self, series: pd.Series):
-        _ = series.value_counts(True)[:(self.topn if self.topn else 10000)]
+        _ = series.value_counts(True)[:self.topn]
         print("Coverage: %.2f %%" % (_.sum() * 100))
-        self.ranker = (_.rank(method='first') - 1).to_dict(OrderedDict)
-        print(self.ranker)
+        self.mapper = (_.rank(method='first') - 1).to_dict(OrderedDict)
+        print(self.mapper)
         return self
 
     def transform(self, series: pd.Series):
-        return series.map(self.ranker)
+        return series.map(self.mapper)
 
 
 if __name__ == '__main__':
