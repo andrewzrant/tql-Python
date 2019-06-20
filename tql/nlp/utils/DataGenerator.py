89f8a83e3e660c6f05a0ce2d8b57d81e7bec3d74
yuanjie <313303303@qq.com>
2019-06-20 21:08:33 +0800

    data gen keras bert

--- tql/nlp/utils/DataGenerator.py
+++ tql/nlp/utils/DataGenerator.py
@@ -6,28 +6,56 @@
 # @Author       : yuanjie
 # @Email        : yuanjie@xiaomi.com
 # @Software     : PyCharm
-# @Description  : 
+# @Description  :
+
+
 from sklearn.utils import shuffle
 
 
 class DataGenerator(object):
 
-    def __init__(self, X, y, batch_size=32):
+    def __init__(self, X, y, batch_size=32, maxlen=None, is_train=1, mapper=lambda *args: args):
+        """
+
+        :param X:
+        :param y:
+        :param batch_size:
+        :param maxlen:
+        :param is_train:
+        :param mapper:
+            # bert keras
+            from functools import partial
+            from keras.preprocessing.sequence import pad_sequences
+            from keras_bert import load_trained_model_from_checkpoint, Tokenizer
+
+            func = partial(pad_sequences, maxlen=self._maxlen)
+            def mapper(X, y):
+                X = list(map(mapper, zip(*map(tokenizer.encode, X))))
+                return X, y
+
+        """
         self.X = X
         self.y = y
         self.batch_size = batch_size
         self.steps = (len(X) + batch_size - 1) // batch_size
+        self._maxlen = maxlen if maxlen else max(map(len, X), 1024)
+        self._is_train = is_train
+
+        assert callable(mapper) is True
+        self.mapper = mapper
 
     def __len__(self):
         return self.steps
 
     def __iter__(self):
-        self.X, self.y = shuffle(self.X, self.y)
+        if self._is_train:
+            self.X, self.y = shuffle(self.X, self.y)
         while 1:
             for i in range(self.steps):
                 idx_s = i * self.batch_size
                 idx_e = (i + 1) * self.batch_size
-                yield self.X[idx_s:idx_e], self.y[idx_s:idx_e]
 
-    def _iter_bert(self):
-        pass
+                X = self.X[idx_s:idx_e]
+                y = self.y[idx_s:idx_e]
+
+                yield self.mapper(X, y)
